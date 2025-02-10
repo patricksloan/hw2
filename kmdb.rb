2dbb6569da9bd64aae477fe7b644f23e7d81f914
Patrick Sloan <patrick.sloan@kellogg.northwestern.edu>
2025-02-10 20:49:51 +0000

    added note

--- kmdb.rb
+++ kmdb.rb
@@ -140,6 +140,8 @@ movie["mpaa_rating"] = "PG-13"
 movie["studio_id"] = warner_bros["id"]
 movie.save
 
+# lol recognize it would have been more effcient to add movies 
+
 # Query movie rows from the Movies table
 batman_begins = Movie.find_by({ "title" => "Batman Begins" })
 dark_knight = Movie.find_by({ "title" => "The Dark Knight" })
